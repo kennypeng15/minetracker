c961bdd85794d38e782081db552a37c6e14216ed
Kenny Peng <kennypeng515@gmail.com>
2023-12-01 18:04:42 -0800

    footer updates; try making gh-pages a devdependency again

--- src/App.js
+++ src/App.js
@@ -299,13 +299,17 @@ export default function App() {
       <div className="footer">
         <p>
           This page is built with the <a href="https://recharts.org/en-US/">Recharts</a> library on top of React.
-          It's hosted on GitHub Pages and deployed with the <a href="https://github.com/gitname/react-gh-pages">react-gh-pages</a> library.
+          It's hosted on GitHub Pages and deployed with GitHub Actions and the <a href="https://github.com/gitname/react-gh-pages">react-gh-pages</a> library.
         </p>
         <p>
           The source code for this page, as well as for the MineTracker project in general, can be found on <a href="https://github.com/kennypeng15">my GitHub page.</a>
         </p>
-        <p>Made by Kenny Peng, 2023</p>
-        <p>teddy was here!</p>
+        <p>
+          Made by Kenny Peng, 2023
+        </p>
+        <p>
+          Teddy was here! :3
+        </p>
       </div>
     </>
   );
